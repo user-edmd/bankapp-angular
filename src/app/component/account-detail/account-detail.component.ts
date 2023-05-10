ed8c7031b6295ea2fde7e960e8e8ea00df880a95
Edmond Basilan <111706719+user-edmd@users.noreply.github.com>
2023-05-10 14:14:02 -0700

    added parameters to getTransactions

--- src/app/component/account-detail/account-detail.component.ts
+++ src/app/component/account-detail/account-detail.component.ts
@@ -27,7 +27,7 @@ export class AccountDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAccount();
-    this.getTransactions();
+    this.getTransactions(this.page);
   }
 
   getAccount(): void {
@@ -36,46 +36,27 @@ export class AccountDetailComponent implements OnInit {
     this.accountService.getAccount(accountIdFromRouter)
       .subscribe(account => this.account = account);
   }
-  getTransactions() {
+
+  getTransactions(page: number) {
     const routeParams = this.route.snapshot.paramMap;
     const accountIdFromRouter = Number(routeParams.get('id'));
-    this.transactionService.getTransactions(accountIdFromRouter, this.size, this.page).subscribe(({content, size, number, totalPages}) => {
+    this.transactionService.getTransactions(accountIdFromRouter, this.size, page).subscribe(({content, size, number, totalPages}) => {
       this.page = number
       this.size = size
       this.total = totalPages
-
-      console.log(content);
-      console.log('Number of Contents '  + size);
-      console.log('Page Number ' + number);
-      console.log('Total pages ' + totalPages);
-
       this.transactions = content;
     });
   }
 
   goToPage(pageSelected: number) {
-    this.page = pageSelected;
-    const routeParams = this.route.snapshot.paramMap;
-    const accountIdFromRouter = Number(routeParams.get('id'));
-    this.transactionService.getTransactions(accountIdFromRouter, this.size, this.page).subscribe(({content, size, number, totalPages}) => {
-      this.page = number
-      this.size = size
-      this.total = totalPages
-      this.transactions = content;
-    });
+    if (this.page !== pageSelected)
+      this.getTransactions(pageSelected);
   }
 
   goToPrevOrNext(buttonSelected: string) {
-    if (this.page >= 0 && this.page < this.total - 1) {
+    if (this.page - 1 >= 0 && this.page + 1 < this.total) {
       buttonSelected === 'prev' ? this.page-- : this.page++
-      const routeParams = this.route.snapshot.paramMap;
-      const accountIdFromRouter = Number(routeParams.get('id'));
-      this.transactionService.getTransactions(accountIdFromRouter, this.size, this.page).subscribe(({content, size, number, totalPages}) => {
-        this.page = number
-        this.size = size
-        this.total = totalPages
-        this.transactions = content;
-      });
+      this.getTransactions(this.page);
    }
   }
 }
