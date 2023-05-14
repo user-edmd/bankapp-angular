4174ce61584b6fa56ea6f396322f100bc8c3bcd7
Edmond Basilan <edbsln@gmail.com>
2023-05-13 23:02:19 -0700

    WIP moved transactions to user detail

--- src/app/component/user-detail/user-detail.component.ts
+++ src/app/component/user-detail/user-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Transaction } from 'src/app/common/transaction';
 import { User } from 'src/app/common/user';
 import { AccountService } from 'src/app/services/account.service';
+import { TransactionService } from 'src/app/services/transaction.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -11,12 +13,19 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class UserDetailComponent implements OnInit {
   user: User | undefined;
+  transactions: Transaction[] = [];
+
+  page: number = 0;
+  size: number = 8;
+  total: number = 0;
+  accId: number = 0;
   
 
   constructor(
     private route: ActivatedRoute,
     private userService: UserService,
-    private accountService: AccountService
+    private accountService: AccountService,
+    private transactionService : TransactionService
   ) {}
 
   ngOnInit(): void {
@@ -36,6 +45,44 @@ export class UserDetailComponent implements OnInit {
     //this.userService.createUser(id, )
 
 
+  }
+
+  openTransactions(accountId : number) {
+    console.log(accountId)
+    this.transactionService.getTransactions(accountId, 5, 0)
+    
+  }
+
+  getTransactions2(accountId: number, page: number) {
+    this.accId = accountId;
+    const routeParams = this.route.snapshot.paramMap;
+    const accountIdFromRouter = Number(routeParams.get('id'));
+    this.transactionService.getTransactions(accountId, this.size, page).subscribe(({content, size, number, totalPages}) => {
+      this.page = number
+      this.size = size
+      this.total = totalPages
+      this.transactions = content;
+    });
+  }
+
+  goToPage(pageSelected: number) {
+    if (this.page !== pageSelected)
+      this.getTransactions2(this.accId, pageSelected);
+  }
+
+  goToPrevOrNext(buttonSelected: string) {
+      if (buttonSelected === 'prev') {
+        if (this.page - 1 >= 0) {
+          this.page--
+          this.getTransactions2(this.accId, this.page);
+        }
+      } else if (buttonSelected === 'next') {
+        if (this.page + 1 < this.total) {
+          this.page++
+          this.getTransactions2(this.accId, this.page);
+        }
+      }
+      
   }
 
 }
